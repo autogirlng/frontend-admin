1033974b0515f0acd3952e8ab7371847c3b68423
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2025-06-12 23:26:34 +0100

    Added the DashboardLayout to the host/onboarding page.

--- src/app/host/onboarding/page.tsx
+++ src/app/host/onboarding/page.tsx
@@ -1,11 +1,14 @@
 import { Suspense } from "react";
 import { FullPageSpinner } from "@/components/shared/spinner";
 import HostOnboardingComponent from "@/components/host/HostOnboarding";
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
 
 export default function HostOnboardingPage() {
   return (
     <Suspense fallback={<FullPageSpinner />}>
-      <HostOnboardingComponent />
+      <DashboardLayout title="Host Onboarding" currentPage="Host">
+        <HostOnboardingComponent />
+      </DashboardLayout>
     </Suspense>
   );
 }
