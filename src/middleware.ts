ecf75686a6b9f223834c4cd0177be96ec1735ef9
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2025-10-31 00:09:59 +0100

    Added middleware.

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  if (pathname === "/login") {
+    if (token) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/dashboard/:path*", "/login"],
+};
